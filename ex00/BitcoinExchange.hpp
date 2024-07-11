327939701673bc540781fdc81536f8304748cfa9
GGabriel003 <108942046+GGabriel003@users.noreply.github.com>
2024-07-11 22:50:37 +0200

    finish ex00

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,23 @@
+#ifndef HEAD_COIN_HPP
+# define HEAD_COIN_HPP
+# include <limits>
+# include <iostream>
+# include <map>
+# include <algorithm>
+# include <fstream>
+# include <sstream>
+# include <iterator>
+
+class BitcoinExchange {
+
+	private:
+		std::map<std::string, double>	 _data;
+		double							getBitcoinRate(const std::string & date) const;
+
+	public:
+		void							readDataBaseFromFile(const std::string & file_name);
+		void							printValueExchanged(const std::string & input_file) const;
+};
+
+
+#endif
